1e32d2818da6a4ab55505b0ceabe767bb2b65888
Ionut Colceriu <contact@ghinda.net>
2015-07-28 12:39:32 +0300

    Built gridlayout from commit 0583692 on branch master

---
+++ js/demos.js
@@ -0,0 +1,70 @@
+/* Demos
+ */
+
+(function() {
+
+  var toggleClass = function(node, className) {
+
+    var foundClassPosition = node.className.indexOf(className);
+
+    if(foundClassPosition === -1) {
+      node.className += ' ' + className;
+    } else {
+
+      var tempClass = node.className.substr(0, foundClassPosition - 1);
+
+      node.className = tempClass + node.className.substr(className.length + foundClassPosition);
+    }
+
+  };
+
+  var toggleDropdown = function(e) {
+
+    var dropdowns = document.querySelectorAll('.dropdown');
+    var i;
+
+    if(e.target.className.indexOf('dropdown-button') === -1) {
+      // close all dropdowns
+      for(i = 0; i < dropdowns.length; i++) {
+        if(dropdowns[i].className.indexOf('dropdown-active') !== -1) {
+          toggleClass(dropdowns[i], 'dropdown-active');
+        }
+      }
+
+      return false;
+    } else {
+
+      e.preventDefault();
+
+      var dropdown = e.target.parentNode;
+
+      toggleClass(dropdown, 'dropdown-active');
+
+
+    }
+
+  };
+
+  var toggleContent = function(e) {
+
+    if(e.target.className.indexOf('js-toggle-content') !== -1) {
+      e.preventDefault();
+
+      var contentNode = document.getElementById(e.target.getAttribute('data-content'));
+
+      toggleClass(contentNode, 'content-show');
+    }
+
+  };
+
+  var init = function() {
+    // init content togglers
+    document.body.addEventListener('click', toggleDropdown);
+
+    // init dropdowns
+    document.body.addEventListener('click', toggleContent);
+  };
+
+  init();
+
+})();
